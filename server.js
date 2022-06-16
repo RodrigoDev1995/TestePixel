383cca14c6eeb43176f3917f064f141693e8b0b1
RodrigoDev1995 <107441456+RodrigoDev1995@users.noreply.github.com>
2022-06-16 00:25:19 -0300

    server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+
 const PORT = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/dist/teste'));
 
